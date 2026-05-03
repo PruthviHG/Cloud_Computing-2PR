a23314817f828320de1bb743b79c39ca52f41884
Pruthvi H G <pruthvirajraj666@gmail.com>
2026-05-04 00:49:43 +0530

    Update create_bucket.sh

--- smart-cloud-project/scripts/create_bucket.sh
+++ smart-cloud-project/scripts/create_bucket.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-gcloud config set project YOUR_PROJECT_ID
+gcloud config set project gen-lang-client-0597944803
 
 gcloud storage buckets create gs://my-project-bucket --location=us-central1
